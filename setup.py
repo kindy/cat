b24e7b8284a396a601cba930e762c7432a8195a0
Kindy Lin <kindy.lin@gmail.com>
2016-06-05 12:48:51 +0800

    init

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='xcat',
+    version='0.1',
+    author='Kindy Lin',
+    url='https://github.com/kindy/xcat',
+    packages=[
+        'xcat',
+    ],
+    scripts=[
+        'bin/xcat',
+    ],
+)
